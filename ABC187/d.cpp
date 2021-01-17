3c2ed798c6126410c6a133691e0d55de31afa0f7
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2021-01-17 18:56:48 +0900

    update 2021.01.17

---
+++ ABC187/d.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REPR(i,n) for(int i=n; i>-1; --i)
+#define ALL(a) (a).begin(),(a).end()
+#define FILL(a,n,x) REP(i,(n)){ (a)[i]=(x); }
+#define CINA(a,n) REP(i,(n)){ cin >> (a)[i]; }
+#define FILL2(a,n,m,x) REP(i,(n)){ REP(j,(m)){(a)[i][j]=(x);} }
+#define CINA2(a,n,m) REP(i,(n)){ REP(j,(m)){cin >> (a)[i][j];} }
+#define Liny "Yes\n"
+#define Linn "No\n"
+#define LINY "YES\n"
+#define LINN "NO\n"
+int keta(ll x){ if(x<10){return 1;} else{return keta(x/10) + 1;}}
+int keta_wa(ll x){ if(x<10){return x;} else{return keta_wa(x/10) + x%10;} }
+int ctoi(char c){ return ( (c>='0' && c<='9')? c - '0': 0 );}
+int __stoi(string s){ return atoi(s.c_str()); }
+ll sum(ll a[],ll N){ return accumulate(a,a+N,0LL);}
+ll gcd(ll a,ll b){if(a<b)swap(a,b); return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){if(a<b){swap(a,b);} return a/gcd(a,b)*b;}
+template<class T> void chmax(T& a, T b){ if(a<b){a=b;} }
+template<class T> void chmin(T& a, T b){ if(a>b){a=b;} }
+template<class T> bool isIn(T a,vector<T> v){ for(T x:v){ if(a==x){return true;}} return false;}
+string strReplace(string s,string target, string replacement){
+    if (!target.empty()) {
+     std::string::size_type pos = 0;
+      while ((pos = s.find(target, pos)) != std::string::npos) {
+        s.replace(pos, target.length(), replacement);
+        pos += replacement.length();
+      }
+    }
+    return s;
+}
+const ll MOD = 998244353;
+using mint = modint998244353;
+// using mint = static_modint<MOD>;
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define MP make_pair
+bool DEBUG = 0;
+template<class T> void dprint(T s){
+  if(DEBUG){ cout << s << "\n"; }
+}
+
+// https://atcoder.jp/contests/abc187/tasks/abc187_d
+
+// 貪欲で行けそう
+
+// どの街でも演説しなかった場合、sum(a) の票差が着く
+// ひとつ街を訪れるごとに b + 2a 票差が縮まる
+
+// RE が出た
+// 先に配列を確保してみる
+
+// TLE が出た
+// 比較関数があやしい？
+// struct を使わずに書いてみる
+
+// またRE
+// 値を先に計算すればバグらないのでは
+// -> 通った
+
+ll x[200005]; 
+
+void solve(){ 
+    int N; cin >> N;
+    ll gap = 0; 
+    REP(i,N){
+        ll a,b; cin >> a >> b;
+        gap += a;
+        x[i] = 2*a + b;
+    }
+    sort(x,x+N,greater<ll>());
+
+    int ans = 0;
+    REP(i,N){
+        ans++;
+        gap -= x[i];
+        if(gap<0) break;
+    }
+    cout << ans << '\n';
+
+}
+int main(int argc, char *argv[]){
+  if(argc>1 && argv[1][0]=='D'){ DEBUG = 1;}
+  solve();
+  return 0;
+}
