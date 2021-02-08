a48e0c3f3ae24707e2f2d3ec52b4caec75dbe664
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2021-02-09 01:39:40 +0900

    update 2021.02.09

---
+++ ABC191/c.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REPR(i,n) for(int i=n; i>-1; --i)
+#define ALL(a) (a).begin(),(a).end()
+#define FILL(a,n,x) REP(i,(n)){ (a)[i]=(x); }
+#define CINA(a,n) REP(i,(n)){ cin >> (a)[i]; }
+#define FILL2(a,n,m,x) REP(i,(n)){ REP(j,(m)){(a)[i][j]=(x);} }
+#define CINA2(a,n,m) REP(i,(n)){ REP(j,(m)){cin >> (a)[i][j];} }
+#define Liny "Yes\n"
+#define Linn "No\n"
+#define LINY "YES\n"
+#define LINN "NO\n"
+int keta(ll x){ if(x<10){return 1;} else{return keta(x/10) + 1;}}
+int keta_wa(ll x){ if(x<10){return x;} else{return keta_wa(x/10) + x%10;} }
+int ctoi(char c){ return ( (c>='0' && c<='9')? c - '0': 0 );}
+int __stoi(string s){ return atoi(s.c_str()); }
+ll sum(ll a[],ll N){ return accumulate(a,a+N,0LL);}
+ll gcd(ll a,ll b){if(a<b)swap(a,b); return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){if(a<b){swap(a,b);} return a/gcd(a,b)*b;}
+template<class T> void chmax(T& a, T b){ if(a<b){a=b;} }
+template<class T> void chmin(T& a, T b){ if(a>b){a=b;} }
+template<class T> bool isIn(T a,vector<T> v){ for(T x:v){ if(a==x){return true;}} return false;}
+string strReplace(string s,string target, string replacement){
+    if (!target.empty()) {
+     std::string::size_type pos = 0;
+      while ((pos = s.find(target, pos)) != std::string::npos) {
+        s.replace(pos, target.length(), replacement);
+        pos += replacement.length();
+      }
+    }
+    return s;
+}
+const ll MOD = 998244353;
+using mint = modint998244353;
+// using mint = static_modint<MOD>;
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define MP make_pair
+bool DEBUG = 0;
+template<class T> void dprint(T s){
+  if(DEBUG){ cout << s << "\n"; }
+}
+
+// https://atcoder.jp/contests/abc191/tasks/abc191_c
+// H,W がともに小さいため、
+// グリッドの各交点が角になり得るか調べる方針で十分そう
+// 周囲 0 ブロックが黒 -> false
+// 周囲 1 ブロックが黒 -> true
+// 周囲 2 ブロックが黒 -> false
+// 周囲 3 ブロックが黒 -> true
+// 周囲 4 ブロックが黒 -> false
+// 偶奇で判定可能っぽい
+
+int H,W;
+string board[15];
+
+void solve(){ 
+    cin >> H >> W;
+    CINA(board,H);
+
+    int ans = 0;
+    for(int gh=1;gh<H;++gh){
+        for(int gw=1;gw<W;++gw){
+            int temp = 0;
+            for(auto p:{MP(gh-1,gw-1),MP(gh-1,gw),MP(gh,gw-1),MP(gh,gw)}){
+                int h = p.first;
+                int w = p.second;
+                temp += (board[h][w]=='#');
+            }
+            ans += temp%2;
+        }
+    }
+    cout << ans << '\n';
+
+}
+int main(int argc, char *argv[]){
+  if(argc>1 && argv[1][0]=='D'){ DEBUG = 1;}
+  solve();
+  return 0;
+}
---
+++ ABC191/d.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REPR(i,n) for(int i=n; i>-1; --i)
+#define ALL(a) (a).begin(),(a).end()
+#define FILL(a,n,x) REP(i,(n)){ (a)[i]=(x); }
+#define CINA(a,n) REP(i,(n)){ cin >> (a)[i]; }
+#define FILL2(a,n,m,x) REP(i,(n)){ REP(j,(m)){(a)[i][j]=(x);} }
+#define CINA2(a,n,m) REP(i,(n)){ REP(j,(m)){cin >> (a)[i][j];} }
+#define Liny "Yes\n"
+#define Linn "No\n"
+#define LINY "YES\n"
+#define LINN "NO\n"
+int keta(ll x){ if(x<10){return 1;} else{return keta(x/10) + 1;}}
+int keta_wa(ll x){ if(x<10){return x;} else{return keta_wa(x/10) + x%10;} }
+int ctoi(char c){ return ( (c>='0' && c<='9')? c - '0': 0 );}
+int __stoi(string s){ return atoi(s.c_str()); }
+ll sum(ll a[],ll N){ return accumulate(a,a+N,0LL);}
+ll gcd(ll a,ll b){if(a<b)swap(a,b); return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){if(a<b){swap(a,b);} return a/gcd(a,b)*b;}
+template<class T> void chmax(T& a, T b){ if(a<b){a=b;} }
+template<class T> void chmin(T& a, T b){ if(a>b){a=b;} }
+template<class T> bool isIn(T a,vector<T> v){ for(T x:v){ if(a==x){return true;}} return false;}
+string strReplace(string s,string target, string replacement){
+    if (!target.empty()) {
+     std::string::size_type pos = 0;
+      while ((pos = s.find(target, pos)) != std::string::npos) {
+        s.replace(pos, target.length(), replacement);
+        pos += replacement.length();
+      }
+    }
+    return s;
+}
+const ll MOD = 998244353;
+ using mint = modint998244353;
+// using mint = static_modint<MOD>;
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define MP make_pair
+bool DEBUG = 0;
+template<class T> void dprint(T s){
+  if(DEBUG){ cout << s << "\n"; }
+}
+
+// https://atcoder.jp/contests/abc191/tasks/abc191_d
+// 浮動小数点数に注意
+// ceil で上側・右側に入る
+// floor で下側・左側に入る
+
+// 愚直にグリッドを数えると 10^10 になって間に合わない
+// 直線でスライスすれば 10^5 に落とせる
+
+const ll UNIT = pow(10,4);
+
+void solve(){ 
+    ld Xf,Yf,Rf; cin >> Xf >> Yf >> Rf;
+    ll X = roundl(Xf*UNIT);
+    ll Y = roundl(Yf*UNIT);
+    ll R = roundl(Rf*UNIT);
+
+    ll start = floorl((X-R) / UNIT);
+    ll end = ceill((X+R) / UNIT);
+
+    ll ans = 0;
+    for(ll x = start; x<=end; ++x){
+        ll pow_temp = R*R - (X-x*UNIT) * (X-x*UNIT);
+        if(pow_temp<0) continue;
+
+        ll temp = sqrtl(pow_temp);
+        ll u = floorl((ld)(Y + temp) / UNIT);
+        ll d = floorl((ld)(Y - temp-1) / UNIT);
+        ans += (u-d);
+    }
+    cout << ans << '\n';
+}
+int main(int argc, char *argv[]){
+  if(argc>1 && argv[1][0]=='D'){ DEBUG = 1;}
+  solve();
+  return 0;
+}
