f614bcc92a4e769928847765bd47e66792d20a5a
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2021-02-19 12:59:36 +0900

    update 2021.02.19

---
+++ ABC189/d.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REPR(i,n) for(int i=n; i>-1; --i)
+#define ALL(a) (a).begin(),(a).end()
+#define FILL(a,n,x) REP(i,(n)){ (a)[i]=(x); }
+#define CINA(a,n) REP(i,(n)){ cin >> (a)[i]; }
+#define FILL2(a,n,m,x) REP(i,(n)){ REP(j,(m)){(a)[i][j]=(x);} }
+#define CINA2(a,n,m) REP(i,(n)){ REP(j,(m)){cin >> (a)[i][j];} }
+#define Liny "Yes\n"
+#define Linn "No\n"
+#define LINY "YES\n"
+#define LINN "NO\n"
+int keta(ll x){ if(x<10){return 1;} else{return keta(x/10) + 1;}}
+int keta_wa(ll x){ if(x<10){return x;} else{return keta_wa(x/10) + x%10;} }
+int ctoi(char c){ return ( (c>='0' && c<='9')? c - '0': 0 );}
+int __stoi(string s){ return atoi(s.c_str()); }
+ll sum(ll a[],ll N){ return accumulate(a,a+N,0LL);}
+ll gcd(ll a,ll b){if(a<b)swap(a,b); return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){if(a<b){swap(a,b);} return a/gcd(a,b)*b;}
+template<class T> void chmax(T& a, T b){ if(a<b){a=b;} }
+template<class T> void chmin(T& a, T b){ if(a>b){a=b;} }
+template<class T> bool isIn(T a,vector<T> v){ for(T x:v){ if(a==x){return true;}} return false;}
+string strReplace(string s,string target, string replacement){
+    if (!target.empty()) {
+     std::string::size_type pos = 0;
+      while ((pos = s.find(target, pos)) != std::string::npos) {
+        s.replace(pos, target.length(), replacement);
+        pos += replacement.length();
+      }
+    }
+    return s;
+}
+const ll MOD = 998244353;
+using mint = modint998244353;
+// using mint = static_modint<MOD>;
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define MP make_pair
+bool DEBUG = 0;
+template<class T> void dprint(T s){
+  if(DEBUG){ cout << s << "\n"; }
+}
+
+// https://atcoder.jp/contests/abc189/tasks/abc189_d
+
+// 遷移パターンを考える
+
+// y_{i-1} が True のとき 
+// * S_{i} が AND なら True or False
+// * S_{i} が OR なら True のみ
+
+// y_{i-1} が False のとき
+// * S_{i} が AND なら False のみ
+// * S_{i} が OR なら True or False
+
+// DP で数え上げられそう
+
+int N;
+ll dp[100][2]; // y_i が j(bool) であるようなパターン数 
+
+void solve(){ 
+  cin >> N;
+  dp[0][true] = 1;
+  dp[0][false] = 1;
+
+  for(int i=1;i<=N;++i){
+    string s; cin >> s;
+    if(s=="AND"){
+      dp[i][true] = dp[i-1][true];
+      dp[i][false] = dp[i-1][false]*2 + dp[i-1][true];
+    }else{
+      dp[i][true] = dp[i-1][true]*2 + dp[i-1][false];
+      dp[i][false] = dp[i-1][false];
+    }
+  }
+  ll ans = dp[N][true];
+  cout << ans << '\n';
+}
+int main(int argc, char *argv[]){
+  if(argc>1 && argv[1][0]=='D'){ DEBUG = 1;}
+  solve();
+  return 0;
+}
