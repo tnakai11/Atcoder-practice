2a72689f8d556fd34af66939123a87b2b1683082
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2021-03-01 22:53:49 +0900

    update 2021.03.01

---
+++ ABC/ABC175/b.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REPR(i,n) for(int i=n; i>-1; --i)
+#define ALL(a) (a).begin(),(a).end()
+#define FILL(a,n,x) REP(i,(n)){ (a)[i]=(x); }
+#define CINA(a,n) REP(i,(n)){ cin >> (a)[i]; }
+#define FILL2(a,n,m,x) REP(i,(n)){ REP(j,(m)){(a)[i][j]=(x);} }
+#define CINA2(a,n,m) REP(i,(n)){ REP(j,(m)){cin >> (a)[i][j];} }
+#define Liny "Yes\n"
+#define Linn "No\n"
+#define LINY "YES\n"
+#define LINN "NO\n"
+int keta(ll x){ if(x<10){return 1;} else{return keta(x/10) + 1;}}
+int keta_wa(ll x){ if(x<10){return x;} else{return keta_wa(x/10) + x%10;} }
+int ctoi(char c){ return ( (c>='0' && c<='9')? c - '0': 0 );}
+int __stoi(string s){ return atoi(s.c_str()); }
+ll sum(ll a[],ll N){ return accumulate(a,a+N,0LL);}
+ll gcd(ll a,ll b){if(a<b)swap(a,b); return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){if(a<b){swap(a,b);} return a/gcd(a,b)*b;}
+template<class T> void chmax(T& a, T b){ if(a<b){a=b;} }
+template<class T> void chmin(T& a, T b){ if(a>b){a=b;} }
+template<class T> bool isIn(T a,vector<T> v){ for(T x:v){ if(a==x){return true;}} return false;}
+string strReplace(string s,string target, string replacement){
+    if (!target.empty()) {
+     std::string::size_type pos = 0;
+      while ((pos = s.find(target, pos)) != std::string::npos) {
+        s.replace(pos, target.length(), replacement);
+        pos += replacement.length();
+      }
+    }
+    return s;
+}
+const ll MOD = 998244353;
+using mint = modint998244353;
+// using mint = static_modint<MOD>;
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define MP make_pair
+bool DEBUG = 0;
+template<class T> void dprint(T s){
+  if(DEBUG){ cout << s << "\n"; }
+}
+// https://atcoder.jp/contests/abc175/tasks/abc175_b
+// 全探索をすると O(N^3) であり N <= 10^2 より通る
+// 三角形の条件 a + b > c && b + c > a && c + a > b
+// a < b < c と縛ると後ろ２つの条件は自明
+// 2分探索を使えば O(N^2 log N) に落とせそうだがオーバーキル
+// 一応やってみる
+// b < c を満たす c の最小インデックスと a + b <= c を満たす c の最小インデックスが必要
+
+void solve(){ 
+    int N; cin >> N;
+    vector<ll> l(N); CINA(l,N); sort(ALL(l));
+    int ans = 0;
+    REP(i,N){
+        for(int j=i+1;j<N;++j){
+            if(l[i]==l[j]) continue;
+            auto x = lower_bound(ALL(l),l[j]+1) - l.begin();
+            auto y = lower_bound(ALL(l),l[i]+l[j]) - l.begin();
+            ans += (y-x);
+        }
+    }
+    cout << ans << '\n';
+}
+int main(int argc, char *argv[]){
+  if(argc>1 && argv[1][0]=='D'){ DEBUG = 1;}
+  solve();
+  return 0;
+}
