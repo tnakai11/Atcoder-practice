2f71a3fac5111ffd4eb4ebc990a542df9d4e2489
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2021-03-07 12:52:41 +0900

    update 2021.03.07

---
+++ ABC/ABC193/d.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REPR(i,n) for(int i=n; i>-1; --i)
+#define ALL(a) (a).begin(),(a).end()
+#define FILL(a,n,x) REP(i,(n)){ (a)[i]=(x); }
+#define CINA(a,n) REP(i,(n)){ cin >> (a)[i]; }
+#define FILL2(a,n,m,x) REP(i,(n)){ REP(j,(m)){(a)[i][j]=(x);} }
+#define CINA2(a,n,m) REP(i,(n)){ REP(j,(m)){cin >> (a)[i][j];} }
+#define Liny "Yes\n"
+#define Linn "No\n"
+#define LINY "YES\n"
+#define LINN "NO\n"
+int keta(ll x){ if(x<10){return 1;} else{return keta(x/10) + 1;}}
+int keta_wa(ll x){ if(x<10){return x;} else{return keta_wa(x/10) + x%10;} }
+int ctoi(char c){ return ( (c>='0' && c<='9')? c - '0': 0 );}
+int __stoi(string s){ return atoi(s.c_str()); }
+ll sum(ll a[],ll N){ return accumulate(a,a+N,0LL);}
+ll gcd(ll a,ll b){if(a<b)swap(a,b); return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){if(a<b){swap(a,b);} return a/gcd(a,b)*b;}
+template<class T> void chmax(T& a, T b){ if(a<b){a=b;} }
+template<class T> void chmin(T& a, T b){ if(a>b){a=b;} }
+template<class T> bool isIn(T a,vector<T> v){ for(T x:v){ if(a==x){return true;}} return false;}
+string strReplace(string s,string target, string replacement){
+    if (!target.empty()) {
+     std::string::size_type pos = 0;
+      while ((pos = s.find(target, pos)) != std::string::npos) {
+        s.replace(pos, target.length(), replacement);
+        pos += replacement.length();
+      }
+    }
+    return s;
+}
+const ll MOD = 998244353;
+using mint = modint998244353;
+// using mint = static_modint<MOD>;
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define MP make_pair
+bool DEBUG = 0;
+template<class T> void dprint(T s){
+  if(DEBUG){ cout << s << "\n"; }
+}
+
+// https://atcoder.jp/contests/abc194/tasks/abc194_d
+// 操作2を無視しても解は変わらないため
+// 「N 個の頂点が全て選ばれるのにかかる操作数の期待値」と読み替えられる
+
+// 解説を見た
+
+// 知らない知識ですね...
+// - 確率 p の事象を成功するまで行うときの試行回数の期待値は 1/p
+
+// M個連結している場合に連結数が増える確率　(N-M)/N
+// 状態ごとに期待値 N/(N-M) を加算していけばOK
+
+void solve(){ 
+    ld N; cin >> N;
+    ld ans = 0.;
+    for(ll m=1;m<N;++m){
+        ans += N/(N-(ld)m);
+    }
+    cout << fixed << setprecision(10) << ans << '\n';
+}
+int main(int argc, char *argv[]){
+  if(argc>1 && argv[1][0]=='D'){ DEBUG = 1;}
+  solve();
+  return 0;
+}
