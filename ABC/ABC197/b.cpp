79a8a9dbb80b6ba7af818c419b32ea984638efee
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2021-04-07 18:44:39 +0900

    update 2021.04.07

---
+++ ABC/ABC197/b.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REPR(i,n) for(int i=n; i>-1; --i)
+#define ALL(a) (a).begin(),(a).end()
+#define FILL(a,n,x) REP(i,(n)){ (a)[i]=(x); }
+#define CINA(a,n) REP(i,(n)){ cin >> (a)[i]; }
+#define FILL2(a,n,m,x) REP(i,(n)){ REP(j,(m)){(a)[i][j]=(x);} }
+#define CINA2(a,n,m) REP(i,(n)){ REP(j,(m)){cin >> (a)[i][j];} }
+#define Liny "Yes\n"
+#define Linn "No\n"
+#define LINY "YES\n"
+#define LINN "NO\n"
+int keta(ll x){ if(x<10){return 1;} else{return keta(x/10) + 1;}}
+int keta_wa(ll x){ if(x<10){return x;} else{return keta_wa(x/10) + x%10;} }
+int ctoi(char c){ return ( (c>='0' && c<='9')? c - '0': 0 );}
+int __stoi(string s){ return atoi(s.c_str()); }
+ll sum(ll a[],ll N){ return accumulate(a,a+N,0LL);}
+ll gcd(ll a,ll b){if(a<b)swap(a,b); return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){if(a<b){swap(a,b);} return a/gcd(a,b)*b;}
+template<class T> void chmax(T& a, T b){ if(a<b){a=b;} }
+template<class T> void chmin(T& a, T b){ if(a>b){a=b;} }
+template<class T> bool isIn(T a,vector<T> v){ for(T x:v){ if(a==x){return true;}} return false;}
+string strReplace(string s,string target, string replacement){
+    if (!target.empty()) {
+     std::string::size_type pos = 0;
+      while ((pos = s.find(target, pos)) != std::string::npos) {
+        s.replace(pos, target.length(), replacement);
+        pos += replacement.length();
+      }
+    }
+    return s;
+}
+const ll MOD = 998244353;
+using mint = modint998244353;
+// using mint = static_modint<MOD>;
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define MP make_pair
+bool DEBUG = 0;
+template<class T> void dprint(T s){
+  if(DEBUG){ cout << s << "\n"; }
+}
+
+// https://atcoder.jp/contests/abc197/tasks/abc197_b
+// 各マスについて一々判定を行なっても制約的にはたぶん間に合うが、記述がややこしい
+// (X,Y) を起点として上下左右に走査すれば高々 O(H+W) ステップで突破可能
+
+void solve(){ 
+    int H,W,X,Y; cin >> H >> W >> X >> Y;
+    --X; --Y;
+    string s[H]; CINA(s,H);
+    int ans = 1;
+    // 上下
+    for(int x=X-1;x>=0;--x){
+        if(s[x][Y]=='#') break;
+        ++ans;
+    }
+    for(int x=X+1;x<H;++x){
+        if(s[x][Y]=='#') break;
+        ++ans;
+    }
+    // 左右
+    for(int y=Y-1;y>=0;--y){
+        if(s[X][y]=='#') break;
+        ++ans;
+    }
+    for(int y=Y+1;y<W;++y){
+        if(s[X][y]=='#') break;
+        ++ans;
+    }
+    cout << ans << '\n';
+}
+int main(int argc, char *argv[]){
+  if(argc>1 && argv[1][0]=='D'){ DEBUG = 1;}
+  solve();
+  return 0;
+}
